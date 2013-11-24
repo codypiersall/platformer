b36b7879eb3ef1fc422503980df23f585456d364
Cody Piersall <cody.piersall@gmail.com>
2013-11-24 12:23:29 -0600

    Use a tilemap instead of drawing everything in pygame.

--- structured_mainloop.py
+++ structured_mainloop.py
@@ -1,4 +1,5 @@
 import pygame
+import tmx
 
 SCREEN_SIZE = (640, 480)
 GRAVITY = 2400
@@ -6,11 +7,11 @@ MAX_FALL_SPEED = 500
 
 class Player(pygame.sprite.Sprite):
     SPEED = 200
-    JUMP_IMPULSE = -600
-    def __init__(self, *groups):
+    JUMP_IMPULSE = -700
+    def __init__(self, location, *groups):
         super().__init__(*groups)
         self.image = pygame.image.load('frog.gif')
-        self.rect = pygame.rect.Rect((320, 240), self.image.get_size())
+        self.rect = pygame.rect.Rect(location, self.image.get_size())
         self.resting = False
         self.dy = 0
         
@@ -23,29 +24,31 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= int(self.SPEED * dt)
         if key[pygame.K_RIGHT]:
             self.rect.x += int(self.SPEED * dt)
-
+ 
         # JUMP!
         if self.resting and key[pygame.K_SPACE]:
             self.dy = self.JUMP_IMPULSE
         self.dy = min(MAX_FALL_SPEED, self.dy + GRAVITY * dt)
         
         self.rect.y += self.dy * dt
+        
         new = self.rect
         self.resting = False
-        
-        for cell in pygame.sprite.spritecollide(self, game.walls, False):
-            cell = cell.rect
-            if last.right <= cell.left and new.right > cell.left:
+        for cell in game.tilemap.layers['triggers'].collide(new, 'blockers'):
+            blockers = cell['blockers']
+            if 'l' in blockers and last.right <= cell.left and new.right > cell.left:
                 new.right = cell.left
-            if last.left >= cell.right and new.left < cell.right:
+            if 'r' in blockers and last.left >= cell.right and new.left < cell.right:
                 new.left = cell.right
-            if last.bottom <= cell.top and new.bottom > cell.top:
+            if 't' in blockers and last.bottom <= cell.top and new.bottom > cell.top:
                 self.resting = True
                 new.bottom = cell.top
                 self.dy = 0
-            if last.top >= cell.bottom and new.top < cell.bottom:
+            if 'b' in blockers and last.top >= cell.bottom and new.top < cell.bottom:
                 new.top = cell.bottom
                 self.dy = 0
+        
+        game.tilemap.set_focus(new.x, new.y)
         
 class Game():
     def main(self, screen):
@@ -53,19 +56,16 @@ class Game():
         
         background = pygame.image.load('Castle.gif')
         background = pygame.transform.scale(background, SCREEN_SIZE)
-        sprites = pygame.sprite.Group()
-        self.player = Player(sprites)
-        self.walls = pygame.sprite.Group()
-        block = pygame.image.load('Chest.gif')
-        for x in range(0, 640, 32):
-            for y in range(0, 480, 32):
-                if x in (0, 640-32) or y in (0, 480-32):
-                    wall = pygame.sprite.Sprite(self.walls)
-                    wall.image = block
-                    wall.rect = pygame.rect.Rect((x,y), block.get_size())
         
+        self.tilemap = tmx.load('map.tmx', screen.get_size())
+        
+        self.sprites = tmx.SpriteLayer()
+        start_cell = self.tilemap.layers['triggers'].find('player')[0]
+        self.player = Player((start_cell.px, start_cell.py), self.sprites)
+        self.tilemap.layers.append(self.sprites)
+
         while True:
-            dt = clock.tick(80) / 1000
+            dt = clock.tick(60) / 1000
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -74,10 +74,9 @@ class Game():
                     return
 
             
-            sprites.update(dt, self)
+            self.tilemap.update(dt, self)
             screen.blit(background, (0,0))
-            self.walls.draw(screen)
-            sprites.draw(screen)
+            self.tilemap.draw(screen)
             pygame.display.flip()
     
 if __name__ == '__main__':
