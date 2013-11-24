061989f43ee145466319da5921acc5528c2b375f
Cody Piersall <cody.piersall@gmail.com>
2013-11-24 14:12:12 -0600

    Make the sprite face left or right.

--- structured_mainloop.py
+++ structured_mainloop.py
@@ -5,15 +5,41 @@ SCREEN_SIZE = (640, 480)
 GRAVITY = 2400
 MAX_FALL_SPEED = 500
 
+class Enemy(pygame.sprite.Sprite):
+    SPEED = 100
+    image = pygame.image.load('Diablos.gif')
+    
+    def __init__(self, location, *groups):
+        super().__init__(*groups)
+        self.rect = pygame.rect.Rect(location, self.image.get_size())
+        self.direction = 1
+        
+    def update(self, dt, game):
+        self.rect.x += int(self.direction * self.SPEED * dt)
+        for cell in game.tilemap.layers['triggers'].collide(self.rect, 'reverse'):
+            if self.direction > 0:
+                self.rect.right = cell.left
+            else:
+                self.rect.left = cell.right
+            self.direction *= -1
+            break
+        
+        if self.rect.colliderect(game.player.rect):
+            game.player.is_dead = True
 class Player(pygame.sprite.Sprite):
     SPEED = 200
     JUMP_IMPULSE = -700
     def __init__(self, location, *groups):
         super().__init__(*groups)
-        self.image = pygame.image.load('frog.gif')
+        self.image = pygame.image.load('Frog-right.gif')
+        self.right_image = self.image
+        self.left_image = pygame.image.load('Frog-left.gif')
+        
         self.rect = pygame.rect.Rect(location, self.image.get_size())
         self.resting = False
         self.dy = 0
+        self.is_dead = False
+        self.direction = 1
         
     def update(self, dt, game):
         # last position
@@ -22,9 +48,12 @@ class Player(pygame.sprite.Sprite):
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
             self.rect.x -= int(self.SPEED * dt)
+            self.image = self.left_image
+            self.direction = -1
         if key[pygame.K_RIGHT]:
             self.rect.x += int(self.SPEED * dt)
- 
+            self.image = self.right_image
+            self.direction = 1
         # JUMP!
         if self.resting and key[pygame.K_SPACE]:
             self.dy = self.JUMP_IMPULSE
@@ -63,7 +92,13 @@ class Game():
         start_cell = self.tilemap.layers['triggers'].find('player')[0]
         self.player = Player((start_cell.px, start_cell.py), self.sprites)
         self.tilemap.layers.append(self.sprites)
-
+        
+        self.enemies = tmx.SpriteLayer()
+        for enemy in self.tilemap.layers['triggers'].find('enemy'):
+            Enemy((enemy.px, enemy.py), self.enemies)
+        
+        self.tilemap.layers.append(self.enemies)
+        
         while True:
             dt = clock.tick(60) / 1000
 
@@ -78,6 +113,9 @@ class Game():
             screen.blit(background, (0,0))
             self.tilemap.draw(screen)
             pygame.display.flip()
+            if self.player.is_dead:
+                print('YOU DIED')
+                return
     
 if __name__ == '__main__':
     pygame.init()
