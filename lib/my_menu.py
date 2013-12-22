14401c81e7c7209fee4dd89866b315b4f01aaf39
Cody Piersall <cody.piersall@gmail.com>
2013-12-22 10:22:37 -0600

    Add submenus to the main menu.

--- lib/my_menu.py
+++ lib/my_menu.py
@@ -5,28 +5,44 @@ if not pygame.display.get_init():
     pygame.display.init()
 
 if not pygame.font.get_init():
-    pygame.font.init()
-    
+    pygame.font.init()    
+
+class ReturnError(Exception):
+    pass
+
 class Menu(object):
     FONT = pygame.font.Font('../coders_crux.ttf', 32)
     SPACE = 10
     UP = pygame.K_UP
     DOWN = pygame.K_DOWN
+    RETURN = pygame.K_RETURN
     BG_COLOR = (0,0,0)
     FONT_COLOR = (255,0,0)
     SELECTOR_COLOR = (0,255,0)
-    def __init__(self, screen, items, font=FONT):
+    DEFAULT_SETTINGS = {'players': 1, 'level': 'maps/map1.tmx'}
+    
+    def __init__(self, screen, items, font=FONT, settings=DEFAULT_SETTINGS):
+        self.settings = settings
         self.screen = screen
         self.items = items
         self.selected = 0
         self.surfaces = []
         self.font = font
+        self.actions = {}
         self.initial_repeat = pygame.key.get_repeat()
-        print(self.initial_repeat)
-        pygame.key.set_repeat(200, 70)
+        self.repeat = (200, 70)
         self.draw()
-        self.mainloop()
-        
+    
+    def add_item(self, item):
+        self.items.append(item)
+    
+    def add_submenu(self, index, menu):
+        menu.settings = self.settings
+        menu.initial_repeat = self.repeat
+        menu.repeat = self.repeat
+
+        self.add_action(index, menu)
+    
     def draw(self):
         self.surfaces = [self.font.render(str(i), 1, self.FONT_COLOR) for i in self.items]
         
@@ -61,18 +77,37 @@ class Menu(object):
                 self.selected = 0
             else:
                 self.selected += 1
-        
-        
-    
 
     def seeya(self):
         """Clean up code when the menu is destroyed."""
         if self.initial_repeat == (0, 0):
             pygame.key.set_repeat()
         else:
-            pygame.key.set_repeat(self.initial_repeat)
-
+            pygame.key.set_repeat(*self.initial_repeat)
+    
+    def on_enter(self):        
+        action = self.actions[self.selected]
+        if isinstance(action, Menu):
+            action.mainloop()
+            
+        elif action == 'return':
+            raise ReturnError
+        
+        elif isinstance(action, (tuple, list)):
+            if action[0] == 'toggle':
+                pass
+            
+    
+    def add_action(self, index, action):
+        """
+        Supported actions:
+            Change a value in the settings dict.
+            Change the displayed item.
+        """
+        self.actions.update({index: action})
+    
     def mainloop(self):
+        pygame.key.set_repeat(*self.repeat)
         pygame.display.update()
         clock = pygame.time.Clock()
         while True:
@@ -89,10 +124,25 @@ class Menu(object):
                     
                     elif e.key == self.UP or e.key == self.DOWN:
                         self.change_select(e.key)
-            
+                    elif e.key == self.RETURN:
+                        try:
+                            self.on_enter()
+                        except ReturnError:
+                            return
+                        
             self.draw()
             pygame.display.update()
+            
+            
 if __name__ == '__main__':
     screen = pygame.display.set_mode((640, 480))
-    menu = Menu(screen, 'this that the other'.split())
+    main_menu = Menu(screen, 'Start Options Quit'.split())
+    options_menu = Menu(screen, 'Levels Players Back'.split())
+    
+    main_menu.add_action(0, 'start')
+    main_menu.add_submenu(1, options_menu)
+    main_menu.add_action(2, 'return')
+    
+    options_menu.add_action(2, 'return')
+    main_menu.mainloop()
     
