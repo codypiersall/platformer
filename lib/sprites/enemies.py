c3b1c25dacc25594730d91d90555f490cfb01e0c
Cody Piersall <cody.piersall@gmail.com>
2014-01-02 22:19:28 -0600

    Make the enemies turn around when they run into the wall.

--- lib/sprites/enemies.py
+++ lib/sprites/enemies.py
@@ -19,10 +19,15 @@ class Knight(BaseSprite):
         image_path = 'images/sprites/enemies/Sentry-left.gif'
         self.image_left = images.load(image_path, convert=False)
         self.image = self.image_left
-        self.image_right = images.load(image_path, flip=(True, False))
+        self.image_right = images.load(image_path, convert=False, flip=(True, False))
         self.rect = pygame.rect.Rect(location, self.image.get_size())
         
-        
+    def set_image(self):
+        if self.direction == self.LEFT:
+            self.image = self.image_left
+        elif self.direction == self.RIGHT:
+            self.image = self.image_right
+                
     def update(self, dt, game):
         self.move(dt, game)
         for cell in game.tilemap.layers['triggers'].collide(self.rect, 'reverse'):
@@ -33,6 +38,9 @@ class Knight(BaseSprite):
             self.direction *= -1
             break
         
+        self.set_image()
+        
+        # kill any player the enemy collides with.
         for player in game.players:
             if not player.invincible and self.rect.colliderect(player.rect):
                 self.hit(player)
